b92f8aac62dcf543dc828e6c06a6cb5fa2aeacf9
Juan Cruz Losano <locha_96-07@hotmail.com>
2019-06-06 16:55:07 -0300

    mutiplexado de displays

---
+++ TP Final/TP/ejem.tpfinal.X/build/default/production/ejem.tpfinal.o.d
@@ -0,0 +1,3 @@
+build/default/production/ejem.tpfinal.o:  \
+	G:/Lo\ Que\ Estaba\ En\ El\ C/FACULTAD\ FCEFyN/Electronica\ Digital\ 2/tp\ final/ejemplo\ a\ seguir/ejem.tpfinal.X/ejem.tpfinal.asm  \
+
---
+++ TP Final/TP/ejem.tpfinal.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ejem.tpfinal.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ejem.tpfinal.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ejem.tpfinal.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ejem.tpfinal.x.tar
