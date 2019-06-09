cc93cf92ca5493ede23cd8783fe3ad5ea066efbb
locxha96 <locha_96-07@hotmail.com>
2019-06-08 21:43:56 -0300

    cambiado nombre de variable, mux anda, comentados txrx tmr1 ADC

--- TP Final/TP/ejem.tpfinal.X/build/default/production/ejem.tpfinal.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/ejem.tpfinal.o:  \
-	G:/Lo\ Que\ Estaba\ En\ El\ C/FACULTAD\ FCEFyN/Electronica\ Digital\ 2/tp\ final/ejemplo\ a\ seguir/ejem.tpfinal.X/ejem.tpfinal.asm  \
-
