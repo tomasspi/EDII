7f0a6d3687068a16350c8d6920d302263e810b25
locxha96 <locha_96-07@hotmail.com>
2019-06-10 14:10:20 -0300

    ASDF

--- TPFinal/TP/ejem.tpfinal.X/build/default/production/tpfinal.o.d
+++ TPFinal/TP/ejem.tpfinal.X/build/default/production/tpfinal.o.d
@@ -1,3 +1,3 @@
 build/default/production/tpfinal.o:  \
-	C:/Users/Daniela/Desktop/FCEFYN/Electronica\ Digital\ 2/github/EDII/TP\ Final/TP/ejem.tpfinal.X/tpfinal.asm  \
+	C:/Users/Daniela/Desktop/FCEFYN/Electronica\ Digital\ 2/github/EDII/TPFinal/TP/ejem.tpfinal.X/tpfinal.asm  \
 
